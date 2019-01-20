bb1f2249112c849845e67a31a195b332540e46e2
SergeyKR <vip.41243@gmail.com>
2019-01-20 22:24:30 +0700

    start work

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const { readLine } = require('./console');
 
 app();
 
-function app () {
+function app() {
     const files = getFiles();
 
     console.log('Please, write your command!');
     readLine(processCommand);
 }
 
-function getFiles () {
+function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
     return filePaths.map(path => readFile(path));
 }
 
-function processCommand (command) {
+function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
